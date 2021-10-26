d48323215aed63259e6ee2caa8f3fc665b8ac127
proguy914629 <gilbert@pt-gss.com>
2021-10-26 07:49:57 +0700

    Fix

--- openrobot/discord_activities/activity.py
+++ openrobot/discord_activities/activity.py
@@ -2,6 +2,7 @@ import datetime
 import aiohttp
 import asyncio
 import typing
+import json
 from .utils import *
 from .errors import *
 from .enums import *
@@ -15,10 +16,12 @@ class DiscordActivity:
 
     async def _request(self, method: str, url: str, **kwargs) -> typing.Union[typing.Dict[str, typing.Any], str]:
         headers = {
-            'Authorization': self.bot_token
+            'Authorization': self.bot_token,
+            'Content-Type': 'application/json'
         }
         
-        headers.update(kwargs.pop('headers', {}))
+        if 'headers' in kwargs:
+            headers.update(kwargs.pop('headers'))
 
         async with aiohttp.ClientSession() as sess:
             for tries in range(self._tries):
@@ -37,6 +40,12 @@ class DiscordActivity:
     async def set_activity(self, channel_id: int, activity_type: ActivityType, *, max_age: datetime.timedelta = None, max_uses: int = 0, temporary: bool = False, unique: bool = False) -> ActivityResult:
         app_id = activity_type.application_id()
 
-        js = await self._request('POST', f'https://discord.com/api/v9/channels/{channel_id}/invites')
+        js = await self._request('POST', f'https://discord.com/api/v9/channels/{channel_id}/invites', data=json.dumps({
+            'max_age': max_age or 0,
+            'max_uses': max_uses,
+            'temporary': temporary,
+            'unique': unique,
+            'target_application_id': app_id
+        }, separators=(',', ':'), ensure_ascii=True))
 
         return ActivityResult(js)
